8fa2ce4635b472ba3fa0cb00ec2cf0e62d92425b
Tanya Hodges <tanyac52003@gmail.com>
2020-06-05 14:01:53 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,14 +7,15 @@ class String
   end
 
   def question?
-
+    self.end_with? ("?")
   end
 
   def exclamation?
-
+    self.end_with? ("!")
   end
 
   def count_sentences
-
+    sentences = self.gsub(".", "142").gsub("?", "142").gsub("!", "142").split("142").reject {|sentence| sentence == ""}
+    sentences.count
   end
 end
