f5e2474049df6691ebaca12a5af4ad39ee57f70f
Tanya Hodges <tanyac52003@gmail.com>
2020-06-05 14:02:07 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with? (".")
   end
 
   def question?
-
+    self.end_with? ("?")
   end
 
   def exclamation?
-
+    self.end_with? ("!")
   end
 
   def count_sentences
-
+    sentences = self.gsub(".", "142").gsub("?", "142").gsub("!", "142").split("142").reject {|sentence| sentence == ""}
+    sentences.count
   end
 end
